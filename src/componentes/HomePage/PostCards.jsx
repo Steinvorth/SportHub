117710778ab28dcf7ef3778f69f75f1650eb3492
Emiliano Steinvorth <75647452+Steinvorth@users.noreply.github.com>
2024-11-18 21:49:42 -0600

    Funcionalidad Ver posts

--- src/componentes/HomePage/PostCards.jsx
+++ src/componentes/HomePage/PostCards.jsx
@@ -1,25 +1,57 @@
-import React from 'react'
-
-//import bootstrap icons
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import React, { useState, useEffect } from 'react';
+import { getPostsPublicos, getUsuarioUsername } from '../supabase/api';
 
 /*
  * Este es un componente para hacer los Bootstrap Cards para los post.
 */
 
 export const PostCards = () => {
+  const [postObj, setPostObj] = useState([]);
+
+  const fetchUser = async (id) => {
+    if (!id) {
+      console.error('Invalid userId:', id);
+      return 'null';
+    }
+    const user = await getUsuarioUsername(id);
+    return user && user.length > 0 ? user[0].UserName : 'null';
+  }
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const posts = await getPostsPublicos();
+
+      console.log(posts);
+
+      //por cada post, buscamos el username del usuario para mostrarlo.
+      const postsWithUsernames = await Promise.all(posts.map(async post => {
+        const username = await fetchUser(post.IdUsuario);
+        return { ...post, user: username };
+      }));
+
+      setPostObj(postsWithUsernames);
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
     <>
-        <div className="card w-100">
+      {
+        postObj.map((post) => (
+          <div className="card w-100" key={post.id}>
             <div className="card-header">
-                @username
+              @{post.user}
             </div>
-            <img src="https://via.placeholder.com/150" className="card-img-top" alt="imagen post"></img>
-            <div className="card-body">                
+            <img src="{https://via.placeholder.com/150}" className="card-img-top" alt="imagen post"></img>
+              <div className="card-body">
                 <i className="bi bi-trophy"></i> {/* Trofeo para simular el Like */}
-                <p className="card-text">hoy fui a caminar</p>
-            </div>
-        </div>
+                <i class="bi bi-chat"></i> {/* Commments */}
+                <p className="card-text">{post.Descripcion}</p>
+              </div>
+          </div>
+        ))
+      }
     </>
-  )
-}
+  );
+};
--- src/componentes/supabase/api.js
+++ src/componentes/supabase/api.js
@@ -1,5 +1,6 @@
 import supabase from './supabase';
 
+//agregar usuario en sign up a tabla usuarios
 export const addUsuario = async (userAuthId, userName) => {
   try {
     const { data, error } = await supabase
@@ -15,6 +16,48 @@ export const addUsuario = async (userAuthId, userName) => {
     return data;
   } catch (error) {
     console.error('Error adding user to Usuarios:', error.message);
+    return null;
+  }
+};
+
+//get usuario
+export const getUsuarioUsername = async (userId) => {
+  if (!userId) {
+    console.error('Invalid userId:', userId);
+    return null;
+  }
+  try {
+    const { data, error } = await supabase
+      .from('Usuarios')
+      .select('UserName')
+      .eq('UserId', userId);
+
+    if (error) {
+      throw error;
+    }
+
+    return data;
+  } catch (error) {
+    console.error('Error getting user from Usuarios:', error.message);
+    return null;
+  }
+};
+
+//get posts publicos
+export const getPostsPublicos = async () => {
+  try {
+    const { data, error } = await supabase
+      .from('Posts')
+      .select('*')
+      .eq('Privacidad', 'TRUE'); //publico es TRUE, privado es FALSE
+
+    if (error) {
+      throw error;
+    }
+
+    return data;
+  } catch (error) {
+    console.error('Error getting public posts:', error.message);
     return null;
   }
 };
